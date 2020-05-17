fd4e3234707bb0ad5d61dd08daa10b358889163d
Andre Cato <dre@fractal.localdomain>
2020-05-17 13:11:51 -0400

    Remove reset off of shift key

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/cato976/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/cato976/keymap.c
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+------|                                                     |------+------+------+------+------+--------|
      * |        |      |MsLeft|MsDown|MsRght|      |                                                     |      |Lclk  |Rclk  |      |      |  Play  |
      * |--------+------+------+------+------+------|                                                     |------+------+------+------+------+--------|
-     * |        |RESET |      |      |      |      |                                                     |      |MSWDN | Prev | Next | Mute |RESET   |
+     * |        |RESET |      |      |      |      |                                                     |      |MSWDN | Prev | Next | Mute |        |
      * `--------+------+------+------+------+------'                                                     `------+------+------+------+------+--------'
      *                 |      |      |                                                                                 |VolUp |VolDn | 
      *                 `-------------'                                                                                 `-------------'
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS, BL_INC,   
             KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_WH_U, KC_WH_L, KC_WH_R, KC_TRNS, BL_DEC,   
             KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                  KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_MPLY,
-            KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_WH_D, KC_MPRV, KC_MNXT, KC_MUTE, RESET,  
+            KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_WH_D, KC_MPRV, KC_MNXT, KC_MUTE, KC_TRNS,  
                        KC_TRNS, KC_TRNS,                                                 KC_VOLU, KC_VOLD, 
                                       KC_TRNS, KC_TRNS,                           KC_TRNS     , KC_WBAK,
                                       KC_TRNS, KC_TRNS,                           KC_TRNS     , KC_TRNS, 
